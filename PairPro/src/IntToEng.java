284982c7917ab32968acb2f2503bdbd2abeeea36
g12949sa <g12949sa@gm.tsuda.ac.jp>
2014-05-09 11:35:50 +0900

    5-1

---
+++ PairPro/src/IntToEng.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class IntToEng {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int input = sc.nextInt();
+		System.out.println(translateEng(input));
+		
+	}
+	
+	static String translateEng(int n) {
+		
+		String[] e= {"zero","one","two","three","four","five","six","seven","eight","nine","ten"};
+		
+		
+		return"";
+	}
+}

b03cb060e853321dc2e1dcdd272d95b3580658a3
g12949sa <g12949sa@gm.tsuda.ac.jp>
2014-05-09 16:17:37 +0900

    5-1-4

--- PairPro/src/IntToEng.java
+++ PairPro/src/IntToEng.java
@@ -12,14 +12,38 @@ public class IntToEng {
 	static String translateEng(int n) {
 		
 		String[] e= {"zero","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"};
-		String[] e2 = {"twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninety","hundred"};
+		String[] e2 = {"","","twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninety","hundred"};
 		String m = "";
-		for(int i=0; i<e.length; i++) {
-			if(n==i)
-			m=e[i];	
-				
+		String l="";
+		String o="";
+		if(n<=10){
+			for(int i=0; i<e.length; i++) {
+				if(n==i)
+					m=e[i];	
+				}
+		}
+		if(n==20||n==30||n==40||n==50||n==60||n==70||n==80||n==90||n==100) {
+			for(int i=0; i<e2.length; i++) {
+				if(n/10==i)
+					m=e2[i];	
+				}
 		}
 		
+		else{
+			int j,k;
+			j = n/10;
+			k = n%10;
+			for(int i=0; i<e.length; i++) {
+				if(k==i)
+					l=e[i];	
+				}
+			for(int i=0; i<e2.length; i++) {
+				if(j==i)
+					o=e2[i];	
+				}
+			
+			m=o+l;
+		}
 		
 		return m;
 	}
